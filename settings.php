48fba0b0ce707ae4b3333328f425cfe774f3a9a1
morganelepine <114992081+morganelepine@users.noreply.github.com>
2023-02-13 16:49:00 +0100

    Settings

--- settings.php
+++ settings.php
@@ -2,16 +2,16 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <title>ReSoC - Paramètres</title> 
+        <title>ReSoC - Paramètres</title>
         <meta name="author" content="Julien Falconnet">
         <link rel="stylesheet" href="style.css"/>
     </head>
+    
     <body>
         
         <?php include('header.php'); ?>
 
         <div id="wrapper" class='profile'>
-
 
             <aside>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
@@ -19,13 +19,16 @@
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez les informations de l'utilisatrice
                         n° <?php echo intval($_GET['user_id']) ?></p>
-
                 </section>
             </aside>
+
             <main>
+
                 <?php
+
+                //Etape 1
                 /**
-                 * Etape 1: Les paramètres concernent une utilisatrice en particulier
+                 * Les paramètres concernent une utilisatrice en particulier
                  * La première étape est donc de trouver quel est l'id de l'utilisatrice
                  * Celui ci est indiqué en parametre GET de la page sous la forme user_id=...
                  * Documentation : https://www.php.net/manual/fr/reserved.variables.get.php
@@ -33,56 +36,59 @@
                  */
                 $userId = intval($_GET['user_id']);
 
-                /**
-                 * Etape 2: se connecter à la base de donnée
-                 */
+
+                //Etape 2: se connecter à la base de donnée
                 $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
 
-                /**
-                 * Etape 3: récupérer le nom de l'utilisateur
-                 */
+
+                //Etape 3: récupérer le nom de l'utilisateur
                 $laQuestionEnSql = "
-                    SELECT users.*, 
-                    count(DISTINCT posts.id) as totalpost, 
-                    count(DISTINCT given.post_id) as totalgiven, 
-                    count(DISTINCT recieved.user_id) as totalrecieved 
-                    FROM users 
-                    LEFT JOIN posts ON posts.user_id=users.id 
-                    LEFT JOIN likes as given ON given.user_id=users.id 
-                    LEFT JOIN likes as recieved ON recieved.post_id=posts.id 
-                    WHERE users.id = '$userId' 
+                    SELECT users.*,
+                    count(DISTINCT posts.id) as totalpost,
+                    count(DISTINCT given.post_id) as totalgiven,
+                    count(DISTINCT recieved.user_id) as totalrecieved
+                    FROM users
+                    LEFT JOIN posts ON posts.user_id=users.id
+                    LEFT JOIN likes as given ON given.user_id=users.id
+                    LEFT JOIN likes as recieved ON recieved.post_id=posts.id
+                    WHERE users.id = '$userId'
                     GROUP BY users.id
                     ";
                 $lesInformations = $mysqli->query($laQuestionEnSql);
-                if ( ! $lesInformations)
-                {
-                    echo("Échec de la requete : " . $mysqli->error);
+                if (! $lesInformations) {
+                    echo "Échec de la requete : " . $mysqli->error;
                 }
                 $user = $lesInformations->fetch_assoc();
 
-                /**
-                 * Etape 4: à vous de jouer
-                 */
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer les valeurs ci-après puiseffacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "</pre>";
-                ?>                
+
+                //Etape 4: à vous de jouer
+                //@todo: afficher le résultat de la ligne ci dessous, remplacer les valeurs ci-après puis effacer la ligne ci-dessous
+                    // echo "<pre>" . print_r($user, 1) . "</pre>";
+                ?>
+                
                 <article class='parameters'>
+
                     <h3>Mes paramètres</h3>
+
                     <dl>
                         <dt>Pseudo</dt>
-                        <dd>Félicie</dd>
+                            <dd><?php echo $user['alias'] ?></dd>
                         <dt>Email</dt>
-                        <dd>felicie@test.org</dd>
+                            <dd><?php echo $user['email'] ?></dd>
                         <dt>Nombre de message</dt>
-                        <dd>42</dd>
+                            <dd><?php echo $user['totalpost'] ?></dd>
                         <dt>Nombre de "J'aime" donnés </dt>
-                        <dd>12</dd>
+                            <dd><?php echo $user['totalgiven'] ?></dd>
                         <dt>Nombre de "J'aime" reçus</dt>
-                        <dd>53</dd>
+                            <dd><?php echo $user['totalrecieved'] ?></dd>
                     </dl>
 
                 </article>
+
             </main>
+
         </div>
+
     </body>
+
 </html>
