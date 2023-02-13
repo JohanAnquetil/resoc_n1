89967a57f5e368a6074fa1a58a67effdde1f6bc1
morganelepine <114992081+morganelepine@users.noreply.github.com>
2023-02-13 15:38:01 +0100

    Refactoring include header

--- news.php
+++ news.php
@@ -6,26 +6,9 @@
         <meta name="author" content="Julien Falconnet">
         <link rel="stylesheet" href="style.css"/>
     </head>
-
     <body>
 
-        <header>
-            <a href='admin.php'><img src="resoc.jpg" alt="Logo de notre réseau social"/></a>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">▾ Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
-            </nav>
-        </header>
+        <?php include('header.php'); ?>
 
         <div id="wrapper">
 
@@ -68,7 +51,7 @@
                   // de l'afficher
                   // Documentation : les exemples https://www.php.net/manual/fr/mysqli.query.php
                   // plus généralement : https://www.php.net/manual/fr/mysqli.query.php
-                 */
+                */
 
                 // Etape 1: Ouvrir une connexion avec la base de donnée.
                 $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
