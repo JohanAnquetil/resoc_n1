000d9e2424fc198762db0815d53f13dbec6a2373
morganelepine <114992081+morganelepine@users.noreply.github.com>
2023-02-14 15:48:41 +0100

    Ajout registration

--- admin.php
+++ admin.php
@@ -18,7 +18,7 @@
         $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
         //verification
         if ($mysqli->connect_errno) {
-            echo("Échec de la connexion : " . $mysqli->connect_error);
+            echo "Échec de la connexion : " . $mysqli->connect_error;
             exit();
         }
         ?>
@@ -33,7 +33,7 @@
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 // Vérification
                 if (! $lesInformations) {
-                    echo("Échec de la requete : " . $mysqli->error);
+                    echo "Échec de la requete : " . $mysqli->error;
                     exit();
                 }
 
@@ -64,7 +64,7 @@
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 // Vérification
                 if (! $lesInformations) {
-                    echo("Échec de la requete : " . $mysqli->error);
+                    echo "Échec de la requete : " . $mysqli->error;
                     exit();
                 }
 
